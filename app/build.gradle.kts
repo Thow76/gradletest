2abb9e41be6bae95e4bad6bb7130b6e42343a4a4
thow76 <thow1976@hotmail.co.uk>
2024-08-10 14:10:43 +0100

    commit_4_7_1_completed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,15 +24,15 @@ android {
         }
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
+//    buildTypes {
+//        release {
+//            isMinifyEnabled = false
+//            proguardFiles(
+//                getDefaultProguardFile("proguard-android-optimize.txt"),
+//                "proguard-rules.pro"
+//            )
+//        }
+//    }
 //    compileOptions {
 //        sourceCompatibility = JavaVersion.VERSION_1_8
 //        targetCompatibility = JavaVersion.VERSION_1_8
