293e2d3916554c4d71c50592e0d4f0eda6b48214
thow76 <thow1976@hotmail.co.uk>
2024-08-17 14:21:09 +0100

    commit_6_1_completed

--- app/src/main/java/com/example/gradletest/MainActivity.kt
+++ app/src/main/java/com/example/gradletest/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.example.auth.presentation.intro.IntroScreen
 import com.example.core.designsystem.RuniqueTheme
 
 
@@ -20,7 +21,9 @@ class MainActivity : ComponentActivity() {
                 Surface (modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.background)
                 {
-                   
+                   IntroScreen {
+
+                   }
                 }
 
 
@@ -29,6 +32,9 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+
+
 
 
 
